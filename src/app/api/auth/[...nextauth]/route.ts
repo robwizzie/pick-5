fda137747367fd1365fa9b684ef1f52eee8e4a19
Rob Wiscount <robertwiscount@gmail.com>
2025-01-24 12:46:11 -0500

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,39 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import clientPromise from '@/lib/mongodb-adapter';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb-adapter";
 
 export const authOptions = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_ID || '',
-			clientSecret: process.env.GOOGLE_SECRET || ''
-		})
-	],
-	adapter: MongoDBAdapter(clientPromise),
-	secret: process.env.NEXTAUTH_SECRET,
-	session: {
-		strategy: 'jwt'
-	},
-	pages: {
-		signIn: '/auth/signin'
-	},
-	callbacks: {
-		session: async ({ session, token }) => {
-			if (session?.user) {
-				session.user.id = token.sub;
-			}
-			return session;
-		},
-		jwt: async ({ token, user, account }) => {
-			if (user) {
-				token.id = user.id;
-			}
-			return token;
-		}
-	}
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
 };
 
+// The Next.js app directory requires you to explicitly define the HTTP methods
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
